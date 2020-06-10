faace547f0224d60a32f09b227cbaebdf83aebd2
adrienchevrier <adrien.chevrierr@gmail.com>
2020-06-10 15:29:29 +0200

    add chart d3

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from './logo.svg';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
+
+
+
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
---
+++ app/client/src/services/keyvaluesService.js
@@ -0,0 +1,12 @@
+import garminService from './services/garminService';
+
+
+const ValidationService = {
+    tot_hours: function(value) {
+        //inspect the value
+    },
+
+    secondValidationMethod: function(value) {
+        //inspect the value
+    }
+};
